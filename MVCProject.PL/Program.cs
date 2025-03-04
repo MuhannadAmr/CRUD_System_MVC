80362b51a21b7cd60da7cc344812b108fc2cae78
MuhannadAmr <mohanedamr2015@gmail.com>
2025-03-04 15:06:27 +0200

    Upgrade to .NET 6.0

--- MVCProject.PL/Program.cs
+++ MVCProject.PL/Program.cs
@@ -1,7 +1,18 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MVCProject.BLL.Interfaces;
+using MVCProject.BLL.Repositories;
+using MVCProject.DAL.Contexts;
+using MVCProject.DAL.Models;
+using MVCProject.PL.MappingProfiles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,14 +24,65 @@ namespace MVCProject.PL
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var Builder = WebApplication.CreateBuilder(args);
+            #region Configure Services That Allow Dependancy Injection
+            Builder.Services.AddControllersWithViews();
+            Builder.Services.AddDbContext<MVCProjectDbContext>(options =>
+            {
+                options.UseSqlServer(Builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            Builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            Builder.Services.AddAutoMapper(M => M.AddProfiles(new List<Profile>()
+            {
+                new DepartmentProfile(), new EmployeeProfile(), new UserProfile(), new RoleProfile()
+            }));
+
+            Builder.Services.AddIdentity<ApplicationUser, IdentityRole>(option =>
+            {
+                option.Password.RequireNonAlphanumeric = true;
+                option.Password.RequireUppercase = true;
+                option.Password.RequireLowercase = true;
+                option.Password.RequireDigit = true;
+            })
+                .AddEntityFrameworkStores<MVCProjectDbContext>()
+                .AddDefaultTokenProviders();
+
+            Builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(option =>
+            {
+                option.LoginPath = "Account/Login";
+                option.AccessDeniedPath = "Home/Error";
+            });
+            #endregion
+            var app = Builder.Build();
+            #region Configure Http Request Pipelines
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Account}/{action=Login}/{id?}");
+            });
+            #endregion
+            app.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+
     }
 }
